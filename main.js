22b9494cb1b9ae31c2cfbe5b0b9f1152b047f9c5
İbrahim Ethem Sancar <ibrahimethemsancar@gmail.com>
2022-03-08 17:31:10 +0300

    last edits

--- main.js
+++ main.js
@@ -1,17 +1,48 @@
 const fs = require("fs");
 const { parse } = require("csv-parse");
-const builder = require('xmlbuilder');
+const builder = require("xmlbuilder");
+var ET = require("elementtree");
+var XML = ET.XML;
+var ElementTree = ET.ElementTree;
 
+//elementree ile xml tagları oluşturma ve text ekleme
+const writeFile = () => {
+  let order = ET.Element("order");
+  let header = ET.SubElement(order, "header");
+  //  console.log(orderList)
+  for (let i = 0; i < headerAtts.length; i++) {
+    let a = ET.SubElement(header, headerAtts[i]);
+    a.text = orderList[i];
+  }
+  let lines = ET.SubElement(order, "lines");
 
+  for (let i = 0; i < itemList.length; i++) {
+    let line = ET.SubElement(lines, "line");
+    let j = 0;
+    for (const key in itemList[i]) {
+      ET.SubElement(line, itemAtts[j]).text = itemList[i][j];
+      j++;
+      if (j == 7) {
+        j = 0;
+      }
+    }
+  }
+
+  let etree = new ElementTree(order);
+  let xml = etree.write({ xml_declaration: false });
+  // console.log(xml);
+
+  fs.writeFile("./output/output.xml", xml, function (err) { //fs modulü ile dosya yazdırma kısmı
+    if (err) throw err;
+  });
+};
 
 let csvData = [];
 let headerAtts = [];
 let itemAtts = [];
 let orderList = [];
 let itemList = [];
-const read = () => {
-  let i = 0;
-
+function read() {
   fs.createReadStream("./input/input.txt", "utf-8")
     .pipe(parse({ delimiter: ":" }))
     .on("data", function (csvrow) {
@@ -21,7 +52,8 @@ const read = () => {
       //do something with csvData
       // console.log(csvData);
       const regex = /;/g;
-      for (i; i < csvData.length; i++) {
+
+      for (i = 0; i < csvData.length; i++) { //row'ları düzenleme ve array içine push etme işlemi
         let sa = csvData[i][0].replace(regex, ",");
         let arr = sa.split(",");
 
@@ -36,16 +68,9 @@ const read = () => {
           itemList.push(arr);
         }
       }
-     // console.log(itemList);
+      // console.log(itemList);
+      writeFile();
     });
-};
-read();
-
-const writeFile=()=>{
-
-
-    fs.writeFile('./output/output.xml',doc,(err)=>{
-        if(err) console.log(err)
-    })
 }
-writeFile()
+
+read();
